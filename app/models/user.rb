0cdffbdc8f8d10bd05c45f64a391d1908b1e162c
Daniel Draper <hello@danieldraper.com.au>
2018-03-10 23:53:23 +1000

    Fix model foreign_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :posts, dependent: :destroy
+  has_many :posts, foreign_key: "author_id", dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :first_name, presence: true
