c515c453d896e7038a69e0bdb968bd54d29b5434
Daniel Draper <hello@danieldraper.com.au>
2018-03-10 10:40:00 +1000

    Add posts controller and views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :fetch_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.ordered
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(permitted_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to @post, notice: t(".success")
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(permitted_params)
+      redirect_to @post, notice: t(".success")
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path, notice: t(".success")
+    else
+      redirect_to posts_path, alert: t(".error")
+    end
+  end
+
+  private
+
+  def fetch_post
+    @post = Post.find(params[:id])
+  end
+
+  def permitted_params
+    params.require(:post).permit(
+      :title,
+      :body,
+    )
+  end
+end
