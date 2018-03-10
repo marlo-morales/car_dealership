c5874c1fe871200dfb0acc35ec65d1a6d2347304
Daniel Draper <hello@danieldraper.com.au>
2018-03-10 21:09:43 +1000

    Add page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title(prefix = nil)
+    key = "#{controller_path.tr("/", ".")}.#{action_name}.page_title"
+    title = t(
+      key,
+      prefix: prefix,
+      site_name: t("site_name"),
+      default: :site_name
+    )
+
+    content_for(:page_title, title)
+  end
 end
