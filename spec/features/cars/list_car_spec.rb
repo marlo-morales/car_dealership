9207715a9891018df95d32fd93543372bd98f1f9
marLox <marlo.p.o.morales@gmail.com>
2021-03-21 15:51:42 +0800

    user_story2: Restructure feature specs for cars

--- spec/features/cars/list_car_spec.rb
+++ spec/features/cars/list_car_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Cars", type: :feature do
+RSpec.describe "Cars - List Car", type: :feature do
   context "for User Story 1" do
     before do
       visit root_path
@@ -59,45 +59,6 @@ RSpec.describe "Cars", type: :feature do
       expect(page).to have_selector ".notification__alert"
       error = "* Model can't be blank"
       expect(page).to have_text I18n.t("cars.save_failed", errors: error)
-    end
-  end
-
-  context "for User Story 2" do
-    let!(:cars) { create_list(:car, 15, seller: create(:user)) }
-
-    before { visit root_path }
-
-    scenario "List cars" do
-      expect(page).to have_selector ".car .car__title", count: 10
-      expect(page).to have_text "Displaying entries 1 - 10 of 15 in total"
-      expect(page).to have_text "1 2 Next › Last »"
-    end
-
-    scenario "Filter list" do
-      car = cars.first
-      filter_make = car.make
-      select filter_make.upcase, from: "filter_by_make", wait: 10
-      click_button "submit_filter"
-      expect(page).to have_selector ".car .car__title", count: Car.where(make: filter_make).size
-      filter_year = car.year
-      select filter_year, from: "filter_by_year"
-      click_button "submit_filter"
-      expect(page).to have_selector ".car .car__title", count: Car.where(make: filter_make, year: filter_year).size
-    end
-
-    scenario "Filter List - no results" do
-      select "APOLLO", from: "filter_by_make", wait: 10
-      click_button "submit_filter"
-      expect(page).to have_selector ".car .car__title", count: 0
-      expect(page).to have_text "No results found for your filter"
-    end
-
-    scenario "Pagination" do
-      expect(page).to have_text "Displaying entries 1 - 10 of 15 in total"
-      expect(page).to have_text "1 2 Next › Last »"
-      click_link "2", wait: 10
-      expect(page).to have_text "Displaying entries 11 - 15 of 15 in total"
-      expect(page).to have_text "« First ‹ Prev 1 2"
     end
   end
 end
